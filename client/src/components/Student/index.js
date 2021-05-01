d6bd3ca6e36f3a857ec156fdc1f7bab0d0bf4f2a
Gita Alekhya Paul <54375111+gitaalekhyapaul@users.noreply.github.com>
2021-05-01 10:57:04 +0530

    ⚡️ add protected routes using context API

---
+++ client/src/components/Student/index.js
@@ -0,0 +1,6 @@
+import Login from "./Login";
+import Signup from "./Signup";
+import Homepage from "./Homepage";
+import Protected from "./Protected";
+import TableView from "./TableView";
+export { Login, Signup, Homepage, Protected, TableView };
